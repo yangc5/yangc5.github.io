d09e37c7df2c31e6923d471dda8c05cc7fdac3ac
Chen Yang <yangc5@mail.uc.edu>
2015-10-24 11:47:59 -0500

    tictactoe done

--- tic_tac_toe/TicTacToe.js
+++ tic_tac_toe/TicTacToe.js
@@ -3,73 +3,79 @@
 $(document).ready(function(){
   var $cells=$('[data-cell]');
   var $playerTurn = 'X';
+  var $win=false;
 
   $cells.click(function(){
     //if gameover or one player won, restart
-    for(var i=0; i<$cells.length; i++) {
-      console.log($($cells[i]).text());
-    }
-    console.log('outside the loop'+checkForWin());
-    if(!gamestillon() || checkForWin()) {
-      console.log(checkForWin());
+    if(gameover() || $win) {
       restart();
     }
 
+    //Do not allow placing mark if cell is already marked
+    if($(this).text()!=='') {
+      alert('Please make another choice, this cell is already marked.');
+      return;
+    }
+
+    //place the mark
     $(this).html($playerTurn);
-    if(gamestillon()){
-      if(checkForWin()) {
-        console.log('inside winning statement'+checkForWin());
-        $('#announce-winner').text('player ' + $playerTurn + ' won!');
-
-
-      }
+    //after mark is placed, check if game is over
+    if(gameover()) {
+      $('#announce-winner').text('Game Over!');
     }
-    else{
-        $('#announce-winner').text('Game Over!');
+    //or if someone won
+    if(checkForWin()) {
+      console.log('inside winning statement'+checkForWin());
+      $('#announce-winner').text('player ' + $playerTurn + ' won!');
+      $win=true;
     }
+
+    //change turn
     $playerTurn = ($playerTurn ==='X') ? 'O' : 'X';
   });
 
   //if restart button is clicked restart.
-  $('button').click(function(){
+  $('.restart').click(function(){
     restart();
   });
 
-  function checkForWin(){
+  function checkForWin() {
     return (horizontalWin() || verticalWin() || diagonalWin());
   }
 
-  function horizontalWin(){
-    return ($('[data-cell="0"]').text()===$('[data-cell="1"]').text()===$('[data-cell="2"]').text()) ||
-           ($('[data-cell="3"]').text()===$('[data-cell="4"]').text()===$('[data-cell="5"]').text()) ||
-           ($('[data-cell="6"]').text()===$('[data-cell="7"]').text()===$('[data-cell="8"]').text());
+  function horizontalWin() {
+    return ($('[data-cell="0"]').text()===$playerTurn && $('[data-cell="1"]').text()===$playerTurn && $('[data-cell="2"]').text()===$playerTurn) ||
+           ($('[data-cell="3"]').text()===$playerTurn && $('[data-cell="4"]').text()===$playerTurn && $('[data-cell="5"]').text()===$playerTurn) ||
+           ($('[data-cell="6"]').text()===$playerTurn && $('[data-cell="7"]').text()===$playerTurn && $('[data-cell="8"]').text()===$playerTurn);
   }
 
-  function verticalWin(){
-    return ($('[data-cell="0"]').text()===$('[data-cell="3"]').text()===$('[data-cell="6"]').text()) ||
-           ($('[data-cell="1"]').text()===$('[data-cell="4"]').text()===$('[data-cell="7"]').text()) ||
-           ($('[data-cell="2"]').text()===$('[data-cell="5"]').text()===$('[data-cell="8"]').text());
+  function verticalWin() {
+    return ($('[data-cell="0"]').text()===$playerTurn && $('[data-cell="3"]').text()===$playerTurn && $('[data-cell="6"]').text()===$playerTurn) ||
+           ($('[data-cell="1"]').text()===$playerTurn && $('[data-cell="4"]').text()===$playerTurn && $('[data-cell="7"]').text()===$playerTurn) ||
+           ($('[data-cell="2"]').text()===$playerTurn && $('[data-cell="5"]').text()===$playerTurn && $('[data-cell="8"]').text()===$playerTurn);
   }
 
-  function diagonalWin(){
-    return ($('[data-cell="0"]').text()===$('[data-cell="4"]').text()===$('[data-cell="8"]').text()) ||
-           ($('[data-cell="2"]').text()===$('[data-cell="4"]').text()===$('[data-cell="6"]').text());
+  function diagonalWin() {
+    return ($('[data-cell="0"]').text()===$playerTurn && $('[data-cell="4"]').text()===$playerTurn && $('[data-cell="8"]').text()===$playerTurn) ||
+           ($('[data-cell="2"]').text()===$playerTurn && $('[data-cell="4"]').text()===$playerTurn && $('[data-cell="6"]').text()===$playerTurn);
   }
 
-  function gamestillon(){
-    var gamestillon=false;
+  function gameover() {
+    var gameover=true;
     for(var i=0; i<$cells.length; i++) {
       if($($cells[i]).text()===''){
-        gamestillon=true;
+        gameover=false;
       }
     }
-    return gamestillon;
+    return gameover;
   }
 
   function restart() {
     for(var i=0; i<$cells.length; i++) {
       $($cells[i]).html('');
     }
+    $win=false;
+    $('#announce-winner').text('');
   }
 
 });
