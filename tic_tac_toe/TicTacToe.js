2a41ad7d4c943c6cf9fff9c55d75c2a42bdb3c45
Chen Yang <yangc5@mail.uc.edu>
2015-10-24 10:53:14 -0500

    refactoring horizontal/diagonal/vertical won

--- tic_tac_toe/TicTacToe.js
+++ tic_tac_toe/TicTacToe.js
@@ -4,14 +4,24 @@ $(document).ready(function(){
   var $cells=$('[data-cell]');
   var $playerTurn = 'X';
 
-
   $cells.click(function(){
+    //if gameover or one player won, restart
+    for(var i=0; i<$cells.length; i++) {
+      console.log($($cells[i]).text());
+    }
+    console.log('outside the loop'+checkForWin());
+    if(!gamestillon() || checkForWin()) {
+      console.log(checkForWin());
+      restart();
+    }
+
     $(this).html($playerTurn);
-    console.log(checkForWin());
-    console.log(gamestillon());
     if(gamestillon()){
       if(checkForWin()) {
+        console.log('inside winning statement'+checkForWin());
         $('#announce-winner').text('player ' + $playerTurn + ' won!');
+
+
       }
     }
     else{
@@ -20,36 +30,46 @@ $(document).ready(function(){
     $playerTurn = ($playerTurn ==='X') ? 'O' : 'X';
   });
 
+  //if restart button is clicked restart.
+  $('button').click(function(){
+    restart();
+  });
+
   function checkForWin(){
     return (horizontalWin() || verticalWin() || diagonalWin());
   }
 
   function horizontalWin(){
-    return ($('[data-cell="0"]').text()===$playerTurn && $('[data-cell="1"]').text()===$playerTurn && $('[data-cell="2"]').text()===$playerTurn) ||
-           ($('[data-cell="3"]').text()===$playerTurn && $('[data-cell="4"]').text()===$playerTurn && $('[data-cell="5"]').text()===$playerTurn) ||
-           ($('[data-cell="6"]').text()===$playerTurn && $('[data-cell="7"]').text()===$playerTurn && $('[data-cell="8"]').text()===$playerTurn);
+    return ($('[data-cell="0"]').text()===$('[data-cell="1"]').text()===$('[data-cell="2"]').text()) ||
+           ($('[data-cell="3"]').text()===$('[data-cell="4"]').text()===$('[data-cell="5"]').text()) ||
+           ($('[data-cell="6"]').text()===$('[data-cell="7"]').text()===$('[data-cell="8"]').text());
   }
 
   function verticalWin(){
-    return ($('[data-cell="0"]').text()===$playerTurn && $('[data-cell="3"]').text()===$playerTurn && $('[data-cell="6"]').text()===$playerTurn) ||
-           ($('[data-cell="1"]').text()===$playerTurn && $('[data-cell="4"]').text()===$playerTurn && $('[data-cell="7"]').text()===$playerTurn) ||
-           ($('[data-cell="2"]').text()===$playerTurn && $('[data-cell="5"]').text()===$playerTurn && $('[data-cell="8"]').text()===$playerTurn);
+    return ($('[data-cell="0"]').text()===$('[data-cell="3"]').text()===$('[data-cell="6"]').text()) ||
+           ($('[data-cell="1"]').text()===$('[data-cell="4"]').text()===$('[data-cell="7"]').text()) ||
+           ($('[data-cell="2"]').text()===$('[data-cell="5"]').text()===$('[data-cell="8"]').text());
   }
 
   function diagonalWin(){
-    return ($('[data-cell="0"]').text()===$playerTurn && $('[data-cell="4"]').text()===$playerTurn && $('[data-cell="8"]').text()===$playerTurn) ||
-           ($('[data-cell="2"]').text()===$playerTurn && $('[data-cell="4"]').text()===$playerTurn && $('[data-cell="6"]').text()===$playerTurn);
+    return ($('[data-cell="0"]').text()===$('[data-cell="4"]').text()===$('[data-cell="8"]').text()) ||
+           ($('[data-cell="2"]').text()===$('[data-cell="4"]').text()===$('[data-cell="6"]').text());
   }
 
   function gamestillon(){
     var gamestillon=false;
     for(var i=0; i<$cells.length; i++) {
-      console.log($cells[i]);
       if($($cells[i]).text()===''){
         gamestillon=true;
       }
     }
     return gamestillon;
+  }
+
+  function restart() {
+    for(var i=0; i<$cells.length; i++) {
+      $($cells[i]).html('');
+    }
   }
 
 });
