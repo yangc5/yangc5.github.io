c3aff68e3359245c95f967b847d21db03288f140
Chen Yang <yangc5@mail.uc.edu>
2015-10-25 11:11:54 -0500

    added blogs, about and contact

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 
 $(document).ready(function(){
   $('.header').load("header.html");
+
+  //Todo: make the following code working!!!
+  $('.external_link').on("click", function(){
+    console.log("This link is for an external site. Want to proceed?");
+  });
 });//end document.ready
