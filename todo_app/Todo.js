5770a2446ac111a5d098f55b00b37bc719064f9b
Chen Yang <yangc5@mail.uc.edu>
2015-10-27 19:23:18 -0500

    append todos to list

--- todo_app/Todo.js
+++ todo_app/Todo.js
@@ -3,7 +3,17 @@
 $(function() {
   $('.header').load("../header.html");
 
-  console.log("Hello.");
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var item = $(this).find('input[type="text"]').val();
+    if(item!==''){
+      var todo='<li>'+item+'</li>';
+      $("#todo-list").append(todo);
+      $(this).find('input[type="text"]').val('');
+    }
+
+
+  });
 
 
 });
