5edc5142784d1d1fc1703a5604707ab82b593ac0
Chen Yang <yangc5@mail.uc.edu>
2015-10-27 20:40:54 -0500

    added checkbox

--- todo_app/Todo.js
+++ todo_app/Todo.js
@@ -7,13 +7,11 @@ $(function() {
     event.preventDefault();
     var item = $(this).find('input[type="text"]').val();
     if(item!==''){
-      var todo='<li>'+item+'</li>';
+      var todo='<li><form class="item"><input type="checkbox" class="clear-item"><label>'+item+'</label></form></li>';
       $("#todo-list").append(todo);
       $(this).find('input[type="text"]').val('');
     }
   });
 
   $('#todo-list').sortable();
-
-
 });
