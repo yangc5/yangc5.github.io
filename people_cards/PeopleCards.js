af9334a358a505787ed7d522fe51bd0e35c2cfdf
Chen Yang <yangc5@mail.uc.edu>
2015-10-29 20:09:28 -0500

    people cards app done

--- people_cards/PeopleCards.js
+++ people_cards/PeopleCards.js
@@ -1,9 +1,27 @@
 "use strict";
 
 $(function(){
+  $('.header').load("../header.html");
+
   $.ajax("https://reqres-api.herokuapp.com/api/users", {
-    success: function(response){
-      console.log(response[0]);
+    success: function(users){
+      users.forEach(function(user){
+        $("#people").append('<li><a class="person" data-href="https://reqres-api.herokuapp.com/api/users/'+user['id']+'">'+user['first_name']+' '+user['last_name']+'</a></li>');
+      });
+
+      $('.person').click(function(e){
+        e.preventDefault();
+        $.ajax($(this).attr('data-href'), {
+          success: function(user){
+            var avatar='<img src='+user['avatar']+ ' alt="avatar">';
+            $('#person-details').html(avatar);
+          }
+        });
+      });
     }
   });
+
+
+
+
 });
