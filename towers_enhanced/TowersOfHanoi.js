f7e22283f6b15f889d64ee72d794de96cd75b969
Chen Yang <yangc5@mail.uc.edu>
2015-10-31 19:48:51 -0500

    enhanced Tower of Hanoi

--- towers_enhanced/TowersOfHanoi.js
+++ towers_enhanced/TowersOfHanoi.js
@@ -2,89 +2,69 @@
 
 $(function(){
 
+  var gameover = false;
   var $stacks = $('[data-stack]');
   var $blocks = $('[data-block]');
+  //since only last child of every stack is allowed to make the move, give them class movable
+  var $movableBlocks = $('[data-block]:last-child');
+  $movableBlocks.addClass("movable");
+
   //any block is draggable, but they rever to original position unless condition is met
   $blocks.draggable({
     revert: true
   });
-  //only when droppable is true, can a block be dropped
+  //only when droppable is true, can a movable block be dropped
   $stacks.droppable({
-    accept: "[data-block]",
+    accept: ".movable",
     drop: function(event, ui) {
-      // if(goodToDrop($(this), ui)) {
-        ui.draggable.draggable('option', 'revert', false);
-        $(this).append(ui.draggable.detach());
-        ui.draggable.css({'top': 0, 'left': 0});
-      // }
+      console.log(goodToDrop($(this), ui.draggable));
+      if(!gameover){
+        if(goodToDrop($(this), ui.draggable)) {
+          ui.draggable.draggable('option', 'revert', false);
+          $(this).append(ui.draggable.detach());
+          ui.draggable.css({'top': 0, 'left': 0});
+          //reset things
+          $movableBlocks = $('[data-block]:last-child');
+          $('[data-block]').removeClass("movable");
+          $movableBlocks.addClass("movable");
+          $blocks.draggable({
+            revert: true
+          });
+          //checkForWin
+          checkForWin();
+        }
+      }
+      else {
+        resetGame();
+      }
     }
   });
 
+  function goodToDrop($stack, $block) {
+    var $last_block = $stack.children().last();
+    if( parseInt($block.attr("data-block"))<parseInt($last_block.attr("data-block")) || $stack.children().length===0) {
+       return true;
+    }
+    else {
+       return false;
+    }
+  }
 
+  function checkForWin() {
+    if($('[data-stack="3"]').children().length===4) {
+      $('#announce-game-won').html("You Won!");
+      gameover=true;
 
+    }
+  }
 
+  function resetGame() {
+    $('[data-stack="1"]').html('<div data-block="100"></div><div data-block="75"></div><div data-block="50"></div><div data-block="25"></div>');
+    $('[data-stack="2"]').empty();
+    $('[data-stack="3"]').empty();
+    $('#announce-game-won').empty();
+    gameover = false;
+  }
 
-  // $stacks.click(move);
-  //
-  // //First check if detached block is empty, if true, detach the last-child of the clicked stack;
-  // //If yes, try dropping.
-  // function move() {
-  //   if (gameover === false) {
-  //     if ($.isEmptyObject($detached)) {
-  //       // if current stack is not empty, detach the last-child of current stack
-  //       if(!($(this).children().length ===0) ) {
-  //         $detached=$(this).children().last().detach();
-  //       }
-  //     }
-  //     else {
-  //       if (dropDetached($(this), $detached)) {
-  //         $detached = {};
-  //       }
-  //     }
-  //     checkForWin();
-  //   }
-  //   else {
-  //     resetGame();
-  //   }
-  // }
-  //
-  // function dropDetached($stack, $detached) {
-  //   //if droppable, drop detached
-  //   //if not, do nothing, return
-  //   if (droppable($stack, $detached)) {
-  //     $stack.append($detached);
-  //     return true;
-  //   }
-  //   else {
-  //     return false;
-  //   }
-  // }
-  //
-  // function droppable($stack, $detached) {
-  //   var $last_block = $stack.children().last();
-  //   if( parseInt($detached.attr("data-block"))<parseInt($last_block.attr("data-block")) || $stack.children().length===0) {
-  //     return true;
-  //   }
-  //   else {
-  //     return false;
-  //   }
-  // }
-  //
-  // function checkForWin() {
-  //   if($('[data-stack="3"]').children().length===4) {
-  //     $('#announce-game-won').html("You Won!");
-  //     gameover=true;
-  //
-  //   }
-  // }
-  //
-  // function resetGame() {
-  //   $('[data-stack="1"]').html('<div data-block="100"></div><div data-block="75"></div><div data-block="50"></div><div data-block="25"></div>');
-  //   $('[data-stack="2"]').empty();
-  //   $('[data-stack="3"]').empty();
-  //   $('#announce-game-won').empty();
-  //   $detached = {};
-  //   gameover = false;
-  // }
 
 });
